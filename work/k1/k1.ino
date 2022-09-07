36a5c60f01fa5f358613d95aa141dfb47ebc255c
Sinho <choi_sinho@naver.com>
2022-09-07 19:12:10 +0900

    0907

---
+++ work/k1/k1.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); //configuration 초기화 함수
+  Serial.println("k2");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//  Serial.println("Hello");
+}
